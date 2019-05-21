42258a518871017c16f4c091c3e873ec24e7e525
汲蛤扑 <song_csu@outlook.com>
2019-05-21 11:39:21 +0800

    Update api.php

--- PHP/Laravel/Laravel文件结构详解/routes/api.php
+++ PHP/Laravel/Laravel文件结构详解/routes/api.php
@@ -16,3 +16,11 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * 模型隐式绑定
+ */
+Route::get('users/{user}', function(App\User $user) {
+  dd($user);
+});
+
