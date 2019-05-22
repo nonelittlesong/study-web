651130b87133a883f1955d60a549e1d9c3475ead
汲蛤扑 <song_csu@outlook.com>
2019-05-22 11:06:12 +0800

    Update Kernel.php

--- PHP/Laravel/Laravel文件结构详解/app/Http/Kernel.php
+++ PHP/Laravel/Laravel文件结构详解/app/Http/Kernel.php
@@ -59,5 +59,6 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
+        // 'token' => CheckToken::class, // 定义指定路由的中间件
     ];
 }
