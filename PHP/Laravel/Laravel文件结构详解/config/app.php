1aec7abd9dfb735dc0cd5980da908ffb66ccd5db
汲蛤扑 <song_csu@outlook.com>
2019-05-24 08:29:13 +0800

    Update app.php

--- PHP/Laravel/Laravel文件结构详解/config/app.php
+++ PHP/Laravel/Laravel文件结构详解/config/app.php
@@ -37,8 +37,7 @@ return [
     | stack traces will be shown on every error that occurs within your
     | application. If disabled, a simple generic error page is shown.
     |
-    | 在开发过程中，应该设置为true
-    | 在生产环境中，应该设置为false
+    | 配置debug模式
     |
     */
 
