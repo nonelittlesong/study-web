4b8c8115f4075d5168632b0614ee570a52874b18
汲蛤扑 <song_csu@outlook.com>
2019-06-20 15:43:12 +0800

    Add files via upload

---
+++ CSS/Bootstrap/GridSystem/DetectComponent.vue
@@ -0,0 +1,105 @@
+<template>
+    <div class="container" style="background: #1d68a7;">
+        <div class="row">
+            <div class="col-md-4">
+                <div class="metroblock buysblock">
+                    <span class="icon fontawesome-ok-sign left"></span>
+                    <h1>{{ getTotalOK }}</h1>
+                    <div class="clearfix"></div>
+                    <h2>OK</h2>
+                </div>
+                <div class="metroblock commentsblock">
+                    <span class="icon fontawesome-warning-sign left"></span>
+                    <h1>{{ getTotalNG }}</h1>
+                    <div class="clearfix"></div>
+                    <h2>NG</h2>
+                </div>
+            </div>
+
+            <div class="col-md-8">
+                <div class="passFail">
+                    {{ getPass }}
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+
+
+</template>
+
+<script>
+    import { mapActions } from 'vuex';
+    import { mapGetters } from 'vuex';
+
+    export default {
+
+        computed: {
+            ...mapGetters([
+                'getTotalOK',
+                'getTotalNG',
+                'getPass'
+            ])
+        },
+
+        methods: {
+            ...mapActions([
+                'loadResult'
+            ])
+        },
+        mounted() {
+            setInterval(this.loadResult, 1000);
+        }
+    }
+</script>
+
+<style>
+    @import url(http://weloveiconfonts.com/api/?family=fontawesome);
+    @import url(https://fonts.googleapis.com/css?family=Open+Sans:400,300);
+
+    /* fontawesome */
+    [class*="fontawesome-"]:before {
+        font-family: 'FontAwesome', sans-serif;
+    }
+    .left {
+        float: left;
+    }
+
+    .buysblock {
+        background: rgb(39,183,121);
+    }
+    .commentsblock {
+        background: #f44336;
+    }
+
+    .metroblock {
+        width: 100%;
+        padding: 0em 1em 1em 1em;
+        color: #fff;
+        font-family: 'Open Sans', sans-serif;
+        margin: 1em;
+    }
+
+    .metroblock h1, .metroblock h2, .metroblock .icon {
+        font-weight: 300;
+        margin: 0;
+        padding: 0;
+    }
+    .metroblock h1, .metroblock .icon {
+        font-size: 7em;
+        text-align: center;
+    }
+    .metroblock .icon {
+        margin-right: .2em;
+    }
+
+    .passFail {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        height: 100%;
+        font-size: 20em;
+    }
+
+</style>
