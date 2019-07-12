0fdb8977cf461f5321b8ccc2ce26d70f29bda4e5
汲蛤扑 <song_csu@outlook.com>
2019-07-12 10:49:44 +0800

    Update api.php

--- PHP/Laravel/Examples/RedisSocket/routes/api.php
+++ PHP/Laravel/Examples/RedisSocket/routes/api.php
@@ -16,3 +16,10 @@ use Illuminate\Http\Request;
 Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+/**
+ * 通过 web 请求触发广播
+ */
+Route::post('/result', function (Request $request) {
+    broadcast(new News($request->getContent()));
+});
