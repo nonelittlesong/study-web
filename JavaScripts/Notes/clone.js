335283609755c289cc36e650d915a168ede63fca
汲蛤扑 <song_csu@outlook.com>
2019-08-15 17:04:07 +0800

    Create clone.js

---
+++ JavaScripts/Notes/clone.js
@@ -0,0 +1,16 @@
+function clone_value(value) {
+  if ( typeof(value) === 'object' ) {
+    if ( Array.isArray(value) ) {
+      return value.slice(0);
+    } else {
+      var copy = {};
+      for ( var p in value ) {
+        if ( value.hasOwnProperty(p) ) {
+          copy[p] = clone_value(value[p]);
+        }
+      }
+      return copy;
+    }
+  }
+  return value;
+}
