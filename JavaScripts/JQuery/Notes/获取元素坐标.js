4365f78c05d7f95d56c36930966015dae9aa6137
汲蛤扑 <song_csu@outlook.com>
2020-06-30 11:04:17 +0800

    Create 获取元素坐标.js

---
+++ JavaScripts/JQuery/Notes/获取元素坐标.js
@@ -0,0 +1,17 @@
+// 参考：
+// https://www.jquery123.com/category/offset/
+// https://blog.csdn.net/tanga842428/article/details/78213427
+
+
+//=============== JQuery 实现 ================
+// 绝对位置
+var x = $('#id').offset().left;
+var y = $('#id').offset().top;
+// 相对父元素位置
+var x = $('#id').position().left;
+var y = $('#id').position().top;
+
+
+// =============== JS 实现 ===============
+// Element.getBoundingClientRect() 方法返回元素的大小及其相对于视口的位置。
+var rect = element.getBoundingClientRect();
