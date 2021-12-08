36df9d8b0ae3bde9860744575396ed974d43bf77
汲蛤扑 <song_csu@outlook.com>
2021-12-08 01:36:16 +0000

    feat(main.sh)

---
+++ Teams/language.md
@@ -0,0 +1,3 @@
+# Teams 开发语言
+
+- [React](../end-front/React)
---
+++ main.sh
@@ -0,0 +1,5 @@
+# git 获取最近提交的时间
+#time=`git show --pretty=format:"%cr" | head -1`
+time=$(git show --pretty=format:"%cr" | head -1)
+
+echo 上次提交：${time}
